f104dee23353ce393e50de78945b22321afa77f6
furkan esen <furkanesen35@gmail.com>
2025-11-15 10:45:01 +0100

    Add timeout to email sending to prevent hanging

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -8,16 +8,24 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  connectionTimeout: 10000, // 10 seconds
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 async function sendMail({ to, subject, text, html }) {
-  return transporter.sendMail({
-    from: process.env.SMTP_USER,
-    to,
-    subject,
-    text,
-    html,
-  });
+  return Promise.race([
+    transporter.sendMail({
+      from: process.env.SMTP_USER,
+      to,
+      subject,
+      text,
+      html,
+    }),
+    new Promise((_, reject) => 
+      setTimeout(() => reject(new Error('Email timeout')), 15000)
+    )
+  ]);
 }
 
 module.exports = { sendMail };
